569407408ae3130e60aa4c6b7b04a3b23d863c97
nsavignano <first@hightheme.com>
2014-08-03 22:25:43 -0600

    first commit

---
+++ jssor/slider-container.js
@@ -0,0 +1,7 @@
+
+$('#slider-container').html('<div>
+                <img u=image src="1024x1024/DSCN0726.jpg" />
+                <div u="thumb">Do you notice it scales while window resize?</div>
+                <div u="caption" t="L" style="position: absolute; top: 20px; left: 20px; width: 200px; height: 30px; color: #ffffff; font-size: 20px; line-height: 30px;">What about caption?</div>
+                <div u="caption" t="CLIP|LR" style="position: absolute; top: 135px; left: 100px; width: 400px; height: 30px; color: #ffffff; font-size: 26px; line-height: 30px; text-align: center;">This is 'CLIP|LR' caption transition</div>
+            </div>');
